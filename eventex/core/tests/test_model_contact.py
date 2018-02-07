b7e55ed58180a2c751ee6d16137bca694ac11836
Diego Maranhão <diegomaranhao@outlook.com>
2018-02-07 19:13:58 +0000

    Contacts

--- eventex/core/tests/test_model_contact.py
+++ eventex/core/tests/test_model_contact.py
@@ -1,20 +1,35 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
-#from eventex.core.models import Speaker, Contact
-"""
+
+# from eventex.core.models import Speaker, Contact
+from eventex.core.models import Speaker, Contact
+
+
 class ContactModelTest(TestCase):
-    def test_create(self):
-        speaker = Speaker.objects.create(
-            name='Diego Maranhao',
-            slug='diego-maranhao',
+    def setUp(self):
+        self.speaker = Speaker.objects.create(
+            name='Henrique Bastos',
+            slug='henrique-bastos',
             photo='http://hbn.link/hb-pic'
         )
 
-        contact = Contact.objects.create(
-            speaker=speaker,
-            kind='E',
-            value='dm@gm.com'
-        )
-
+    def test_create(self):
+        contact = Contact.objects.create(speaker=self.speaker, kind=Contact.EMAIL,
+                                         value='dm@gm.com')
         self.assertTrue(Contact.objects.exists())
 
-"""
+    def test_phone(self):
+        contact = Contact.objects.create(speaker=self.speaker, kind=Contact.PHONE,
+                                         value='3242321232')
+        self.assertTrue(Contact.objects.exists())
+
+    def test_choices(self):
+        """Contact kind should be limited to E or P"""
+        contact = Contact(speaker=self.speaker, kind='A', value='B')
+
+        self.assertRaises(ValidationError, contact.full_clean)
+
+    def test_str(self):
+        contact = Contact(speaker=self.speaker, kind=Contact.EMAIL,
+                                         value='dm@gm.com')
+        self.assertEqual('dm@gm.com', str(contact))
